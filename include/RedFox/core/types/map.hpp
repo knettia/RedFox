b36340564e85995d34635079f88508c8063b78f7
valken v <debian.valken@protonmail.com>
2025-03-03 14:23:48 +0200

    map inline file

--- include/RedFox/core/types/map.hpp
+++ include/RedFox/core/types/map.hpp
@@ -23,63 +23,16 @@ namespace RF
 		// constructor/destructor instructions:
 		map() = default;
 		~map() = default;
-                map(std::initializer_list<std::pair<T1, T2>> list)
-		{
-			for (const auto &[key, value] : list)
-			{
-				this->forward_map_.emplace(key, value);
-				this->reverse_map_.emplace(value, key);
-			}
-                }
+                map(std::initializer_list<std::pair<T1, T2>> list);
 
-		[[nodiscard]] std::optional<T2> operator[](T1 key) const
-		{
-			auto it = this->forward_map_.find(key);
-			if (it != this->forward_map_.end())
-			{
-				return it->second;
-			}
-			return std::nullopt;
-		}
+		[[nodiscard]] std::optional<T2> operator[](T1 key) const;
 
-		[[nodiscard]] std::optional<T1> operator[](T2 key) const
-		{
-			auto it = this->reverse_map_.find(key);
-			if (it != this->reverse_map_.end())
-			{
-				return it->second;
-			}
-			return std::nullopt;
-		}
+		[[nodiscard]] std::optional<T1> operator[](T2 key) const;
 
-		bool insert(T1 k, T2 v)
-		{
-			if (
-				this->forward_map_.find(k) == this->forward_map_.end() &&
-				this->reverse_map_.find(v) == this->reverse_map_.end()
-			)
-			{
-				this->forward_map_.emplace(k, v);
-				this->reverse_map_.emplace(v, k);
-				return true;
-			}
+		bool insert(T1 k, T2 v);
 
-			return false;
-		}
-
-		bool exsert(T1 k, T2 v)
-		{
-			if (
-				this->forward_map_.find(k) != this->forward_map_.end() &&
-				this->reverse_map_.find(v) != this->reverse_map_.end()
-			)
-			{
-				// remove k from forward_map_ and v from reverse_map_
-				this->forward_map_.erase(k);
-				this->reverse_map_.erase(v);
-				return true;
-			}
-			return false;
-		}
+		bool exsert(T1 k, T2 v);
 	};
 } // namespace RF
+
+#include "map.ipp"
