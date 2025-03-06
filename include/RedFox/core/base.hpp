4d8ba2a07173e0da8bf7a36b6c147999352c05ea
Valken <debian.valken@protonmail.com>
2025-03-06 19:05:33 +0200

    header restructure

--- include/RedFox/core/base.hpp
+++ include/RedFox/core/base.hpp
@@ -5,8 +5,3 @@
 // TYPES:
 #include "RedFox/core/types/vector/vec2.hpp" // RF::vec<2, T> and various typedefs
 #include "RedFox/core/types/vector/vec3.hpp" // RF::vec<3, T> and various typedefs
-#include "RedFox/core/types/memory.hpp"      // RF::memory_t<scale> for networking and RF::pool<T>
-#include "RedFox/core/types/mass.hpp"        // RF::mass<T, n>
-#include "RedFox/core/types/pool.hpp"        // RF::pool<T>
-
-#include "RedFox/core/types/library.hpp"     // RF::library abstract layer for Unix dlopen and Windows LoadLibrary
