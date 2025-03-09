66f8c82cee49ed03b956d7f72586960e7a4479a9
Valken <debian.valken@protonmail.com>
2025-03-09 13:32:24 +0200

    implement dialogues

---
+++ src/core/monitor/cocoa_dialogue.mm
@@ -0,0 +1,39 @@
+#if defined (__APPLE__)
+#import "Cocoa/Cocoa.h"
+#import "RF/monitor.hpp"
+
+void RF::monitor_m::crash_dialogue_()
+{
+	NSAlert* alert = [[[NSAlert alloc] init] autorelease];
+
+	[alert setMessageText:@"RedFox Engine: Process Crashed"];
+
+	[alert setInformativeText:@"Oh no! A crash has occurred. We are sorry for the inconvenience, for more information, check the crash log."];
+
+	[alert setAlertStyle:NSAlertStyleCritical];
+
+	[alert addButtonWithTitle:@"Yes"];
+	[alert addButtonWithTitle:@"No"];
+
+	[[alert buttons][1] setKeyEquivalent:@"\r"];
+
+	NSWindow* alert_window = [alert window];
+	[alert_window setLevel:NSModalPanelWindowLevel];
+	[alert_window makeKeyAndOrderFront:nil];
+	[NSApp activateIgnoringOtherApps:YES];
+
+	NSScreen* main_screen = [NSScreen mainScreen];
+	NSRect screen_frame = [main_screen frame];
+	NSRect window_frame = [alert_window frame];
+
+	CGFloat x = NSMidX(screen_frame) - (window_frame.size.width / 2);
+	CGFloat y = NSMidY(screen_frame) - (window_frame.size.height / 2);
+	[alert_window setFrameOrigin:NSMakePoint(x, y)];
+
+	NSModalResponse response = [alert runModal];
+
+	if (response == NSAlertFirstButtonReturn)
+	{ RF::monitor_m::open_save_path_(); }
+}
+
+#endif // __APPLE__
---
+++ src/core/monitor/win32_dialogue.cpp
@@ -0,0 +1,18 @@
+#if defined (_WIN32)
+#include "RF/monitor.hpp"
+
+void RF::monitor_m::crash_dialogue_()
+{
+	int result = MessageBoxA(
+		NULL,
+		"Oh no! A crash has occurred. We are sorry for the inconvenience, for more information, check the crash log.",
+		"RedFox Engine: Process Crashed",
+		MB_ICONERROR | MB_YESNO | MB_DEFBUTTON2
+	);
+	
+	if (result == IDYES)
+	{ RF::monitor_m::open_save_path_(); }
+	else if (result == IDNO)
+	{ PostQuitMessage(0); }
+}
+#endif // _WIN32
