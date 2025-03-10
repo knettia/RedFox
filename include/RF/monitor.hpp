47729d5759a25d5b091419714c105fa3ac387002
Valken <debian.valken@protonmail.com>
2025-03-10 12:03:07 +0200

    refactored monitor_m

--- include/RF/monitor.hpp
+++ include/RF/monitor.hpp
@@ -21,7 +21,14 @@ namespace RF
 		static std::string save_path_;
 		static void open_save_path_();
 		static void crash_dialogue_();
-		
+
+		static void exception_dialogue_(std::string_view title, std::string_view description);
+		static std::string get_timestamp_();
+		static std::string get_platform_name_();
+		static std::ostringstream generate_stack_trace_();
+		static std::ostringstream generate_crash_log_(int signal);
+		static bool handle_exception_();
+
 		#if defined (__linux__) || defined (__APPLE__)
 		static void handle_crash_(int signal);
 		#elif defined (_WIN32)
--- src/core/monitor.cpp
+++ src/core/monitor.cpp
@@ -3,7 +3,6 @@
 #include "RF/monitor.hpp"
 #include "RF/system.hpp"
 #include "RF/string.hpp"
-#include "RF/log.hpp"
 
 #if defined (__linux__) || defined (__APPLE__)
 #include <csignal>
@@ -76,6 +75,8 @@ std::unordered_map<int, std::string> unix_signal_map
 };
 #endif
 
+#include "RF/exception.hpp"
+
 // format:
 /*
  * Process: process
@@ -104,107 +105,178 @@ void RF::monitor_m::handle_crash_(int signal)
 LONG __cdecl RF::monitor_m::handle_crash_(EXCEPTION_POINTERS *exception_info)
 #endif
 {
+	// disable ABORT signal
 	#if defined (__linux__) || defined (__APPLE__)
 	std::signal(SIGABRT, nullptr);
 	#endif
-	
-	std::ostringstream log_stream;
-	
-	RF::sys::memory_data_t process_memory = RF::sys::get_process_memory();
-	
-	log_stream << RF::format_view("Process: <0>", RF::sys::get_process_name()) << '\n';
-	
-	std::string platform;
-	#if defined (__linux__)
-	platform = RF::format_view("Linux <0>", RF::sys::get_distro_name()) ;
-	#elif defined (__APPLE__)
-	platform = "macOS (OS X)";
-	#elif defined (_WIN32)
-	platform = "Windows";
-	#endif
-	
-	log_stream << RF::format_view("Platform: <0>", platform) << '\n';
-	
-	RF::sys::utc_time_t time = RF::sys::get_current_time();
-	log_stream << RF::format_view(
-		"Time: <0>-<1>-<2>T<3>:<4>:<5>.<6>Z",
-		RF::int_to_string(time.year,        4),
-		RF::int_to_string(time.month,       2),
-		RF::int_to_string(time.day,         2),
-		RF::int_to_string(time.hour,        2),
-		RF::int_to_string(time.minute,      2),
-		RF::int_to_string(time.second,      2),
-		RF::int_to_string(time.millisecond, 3)
-	) << '\n';
-	
-	log_stream << '\n';
-	
-	RF::sys::cpu_info_t cpu_info = RF::sys::get_cpu_info();
-	
-	log_stream << RF::format_view("CPU : | Model: <0>", cpu_info.model) << '\n';
-	log_stream << RF::format_view("      | Clock Speed: <0>", cpu_info.clock_speed) << '\n';
-	log_stream << RF::format_view("      | Physical Cores: <0>", static_cast<std::uint16_t>(cpu_info.physical_cores)) << '\n';
-	log_stream << RF::format_view("      | Logical Cores: <0>", static_cast<std::uint16_t>(cpu_info.logical_cores)) << '\n';
-	log_stream << RF::format_view("      | Architecture: <0>", cpu_info.architecture) << '\n';
-	
-	log_stream << '\n';
-	
-	#if defined (__APPLE__)
-	log_stream << RF::format_view("Virtual Memory Usage: <0> GiB", RF::double_to_string(RF::memory_cast<RF::gibibyte_scale>(process_memory.virtual_size).count(), 2)) << '\n';
-	#else
-	log_stream << RF::format_view("Virtual Memory Usage: <0> MiB", RF::double_to_string(RF::memory_cast<RF::mebibyte_scale>(process_memory.virtual_size).count(), 2)) << '\n';
-	#endif
-	log_stream << RF::format_view("Physical Memory Usage: <0> MiB", RF::double_to_string(RF::memory_cast<RF::mebibyte_scale>(process_memory.physical_size).count(), 2)) << '\n';
 
-	log_stream << '\n';
-	
-	#if defined (__linux__) || defined (__APPLE__)
-	log_stream << RF::format_view("UNIX Terminate Signal: <0>", unix_signal_map.at(signal)) << '\n';
-
-	log_stream << '\n';
-	#endif
-
-	RF::sys::stack_trace_t stack_trace = RF::sys::get_stack_trace();
-
-	log_stream << "Stack Trace:" << '\n';
-
-	std::size_t max_frame_width = 0;
-	std::size_t max_process_width = 0;
-
-	for (RF::sys::stack_entry_t entry : stack_trace)
+	if (RF::monitor_m::handle_exception_())
 	{
-		max_frame_width = std::max(max_frame_width, static_cast<std::size_t>(std::to_string(entry.frame).size()));
-		max_process_width = std::max(max_process_width, entry.process.size());
+		// exit programme
+		#if defined (__linux__) || defined (__APPLE__) || defined (__FreeBSD__)  || defined (__OpenBSD__)
+		std::abort();
+		#elif defined (_WIN32)
+		return EXCEPTION_EXECUTE_HANDLER; 
+		#endif
 	}
 
-	for (RF::sys::stack_entry_t entry : stack_trace)
-	{
-		log_stream
-		<< "    | "
-		<< std::right
-		<< std::setw(max_frame_width) << static_cast<int>(entry.frame)
-		<< "  "
-		<< std::left
-		<< std::setw(max_process_width) << entry.process
-		<< std::right
-		<< " :    "
-		<< "0x" << std::setw(16) << std::setfill('0') << std::hex << entry.address
-		<< std::dec << std::setfill(' ')
-		<< " "
-		<< entry.callname
-		<< "\n";
-	}
+	// generate crash log and save it
+	std::ostringstream log = generate_crash_log_(signal);
+	RF::file_m::write_file(RF::monitor_m::save_path_, log.str(), true);
 
-	RF::logf::error(log_stream.str());
-
-	RF::file_m::write_file(RF::monitor_m::save_path_, log_stream.str(), true);
-
+	// show crash dialogue
 	RF::monitor_m::crash_dialogue_();
 
+	// exit programme
 	#if defined (__linux__) || defined (__APPLE__) || defined (__FreeBSD__)  || defined (__OpenBSD__)
 	std::abort();
 	#elif defined (_WIN32)
-
 	return EXCEPTION_EXECUTE_HANDLER; 
 	#endif
+}
+
+bool RF::monitor_m::handle_exception_()
+{
+	try
+	{
+		if (std::exception_ptr eptr = std::current_exception())
+		{
+			std::rethrow_exception(eptr);
+		}
+	}
+	catch (const RF::engine_error &e)
+	{
+		RF::sys::stack_entry_t entry = e.entry();
+		RF::monitor_m::exception_dialogue_(
+			"RedFox Engine Error",
+			RF::format_view("<0>\n\nAt: <1> <2>", e.what(), RF::int_to_hex(entry.address, 16), entry.callname)
+		);
+
+		return true;
+	}
+	catch (const RF::runtime_error &e)
+	{
+		RF::sys::stack_entry_t entry = e.entry();
+		RF::monitor_m::exception_dialogue_(
+			"RedFox Runtime Error",
+			RF::format_view("<0>\n\nAt: <1> <2>", e.what(), RF::int_to_hex(entry.address, 16), entry.callname)
+		);
+
+		return true;
+	}
+	catch (const std::runtime_error &e)
+	{
+		RF::monitor_m::exception_dialogue_("STD Runtime Error", e.what());
+		return true;
+	}
+	catch (...)
+	{ /* do nothing */ }
+
+	return false;
+}
+
+std::ostringstream RF::monitor_m::generate_crash_log_(int signal)
+{
+	std::ostringstream log_stream;
+
+	// general details
+	log_stream << RF::format_view("Process: <0>", RF::sys::get_process_name()) << '\n';
+	log_stream << RF::format_view("Platform: <0>", RF::monitor_m::get_platform_name_()) << '\n';
+	log_stream << RF::format_view("Time: <0>", RF::monitor_m::get_timestamp_()) << '\n';
+
+	log_stream << '\n';
+
+	// CPU information
+	RF::sys::cpu_info_t cpu_info = RF::sys::get_cpu_info();
+	log_stream << RF::format_view("CPU : | Model: <0>", cpu_info.model) << '\n'
+	           << RF::format_view("      | Clock Speed: <0>", cpu_info.clock_speed) << '\n'
+	           << RF::format_view("      | Physical Cores: <0>", static_cast<std::uint16_t>(cpu_info.physical_cores)) << '\n'
+	           << RF::format_view("      | Logical Cores: <0>", static_cast<std::uint16_t>(cpu_info.logical_cores)) << '\n'
+	           << RF::format_view("      | Architecture: <0>", cpu_info.architecture) << '\n';
+
+	log_stream << '\n';
+
+	// memory usage
+	RF::sys::memory_data_t process_memory = RF::sys::get_process_memory();
+
+	#if defined (__DARWIN_ALIAS) // DARWIN virtual memory is larger, measure in GiB
+	log_stream << RF::format_view("Virtual Memory Usage: <0> GiB",
+	              RF::double_to_string(RF::memory_cast<RF::gibibyte_scale>(process_memory.virtual_size).count(), 2)) << '\n';
+	#else
+	log_stream << RF::format_view("Virtual Memory Usage: <0> MiB",
+	              RF::double_to_string(RF::memory_cast<RF::mebibyte_scale>(process_memory.virtual_size).count(), 2)) << '\n';
+	#endif
+	log_stream << RF::format_view("Physical Memory Usage: <0> MiB",
+	              RF::double_to_string(RF::memory_cast<RF::mebibyte_scale>(process_memory.physical_size).count(), 2)) << '\n';
+
+	log_stream << '\n';
+
+	// UNIX terminate signal
+	#if defined (__linux__) || defined (__APPLE__)
+	log_stream << RF::format_view("UNIX Terminate Signal: <0>", unix_signal_map.at(signal)) << '\n';
+	log_stream << '\n';
+	#endif
+
+	// stack trace
+	log_stream << RF::monitor_m::generate_stack_trace_().str();
+
+	return log_stream;
+}
+
+std::ostringstream RF::monitor_m::generate_stack_trace_()
+{
+	std::ostringstream log_stream;
+
+	log_stream << "Stack Trace:" << '\n';
+
+	RF::sys::stack_trace_t stack_trace = RF::sys::get_stack_trace();
+
+	// find column widths for formatting
+	std::size_t max_frame_width = 0;
+	std::size_t max_process_width = 0;
+	for (const RF::sys::stack_entry_t &entry : stack_trace)
+	{
+		max_frame_width = std::max(max_frame_width, std::to_string(entry.frame).size());
+		max_process_width = std::max(max_process_width, entry.process.size());
+	}
+
+	// append stack trace entries
+	for (const RF::sys::stack_entry_t &entry : stack_trace)
+	{
+		log_stream << "    | "
+		           << std::right << std::setw(max_frame_width) << entry.frame << "  "
+		           << std::left << std::setw(max_process_width) << entry.process
+		           << RF::format_view(" :    <0> <1>", RF::int_to_hex(entry.address, 16), entry.callname)
+		           << "\n";
+	}
+
+	return log_stream;
+}
+
+std::string RF::monitor_m::get_platform_name_()
+{
+	#if defined (__linux__)
+	return RF::format_view("Linux <0>", RF::sys::get_distro_name());
+	#elif defined (__APPLE__)
+	return "macOS (OS X)";
+	#elif defined (_WIN32)
+	return "Windows";
+	#else
+	return "Unknown Platform";
+	#endif
+}
+
+std::string RF::monitor_m::get_timestamp_()
+{
+	RF::sys::utc_time_t time = RF::sys::get_current_time();
+	return RF::format_view(
+		"<0>-<1>-<2>T<3>:<4>:<5>.<6>Z",
+		RF::int_to_string(time.year, 4),
+		RF::int_to_string(time.month, 2),
+		RF::int_to_string(time.day, 2),
+		RF::int_to_string(time.hour, 2),
+		RF::int_to_string(time.minute, 2),
+		RF::int_to_string(time.second, 2),
+		RF::int_to_string(time.millisecond, 3)
+	);
 }
