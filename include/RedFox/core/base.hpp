d4cd46d8bab004fb8c8af897bab175f4dd9b5092
Valken <debian.valken@protonmail.com>
2025-03-24 13:36:51 +0200

    removed vector types from common include

--- include/RedFox/core/base.hpp
+++ include/RedFox/core/base.hpp
@@ -1,7 +1,3 @@
 #pragma once
 
 #include "RedFox/core/utils/ignoramus.hpp" // engine logging RF::ignoramus::logf
-
-// TYPES:
-#include "RedFox/core/types/vector/vec2.hpp" // RF::vec<2, T> and various typedefs
-#include "RedFox/core/types/vector/vec3.hpp" // RF::vec<3, T> and various typedefs
