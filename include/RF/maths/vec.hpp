b3356c3efbe155c0d48924c38f1a63d28a2a3db8
Valken <debian.valken@protonmail.com>
2025-04-18 18:00:41 +0300

    created vec_guard

--- include/RF/maths/vec.hpp
+++ include/RF/maths/vec.hpp
@@ -4,8 +4,18 @@
 #include "RF/template.hpp" // RF_arithmetic_template
 #include "RF/exception.hpp" // RF::runtime_error
 
+namespace RF::detail
+{
+	template<int v>
+	struct vec_guard
+	{
+		static constexpr bool valid = (v >= 2 && v <= 4);
+	};
+} // namespace RF::detail
+
 namespace RF
 {
 	template<int n, typename A, RF_arithmetic_template(A)>
+	requires(detail::vec_guard<n>::valid)
 	struct vec;
-}
+} // namespace RF
