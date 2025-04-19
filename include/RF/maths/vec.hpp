623ee9ac5608c2ffef0ef9f2a58f0d894d866482
Valken <debian.valken@protonmail.com>
2025-04-19 12:11:21 +0300

    moved vector headers to new vec folder

--- include/RF/maths/vec.hpp
+++ include/RF/maths/vec.hpp
@@ -1,21 +1,6 @@
 #pragma once
 
-#include <cmath>
-#include "RF/template.hpp" // RF_arithmetic_template
-#include "RF/exception.hpp" // RF::runtime_error
-
-namespace RF::detail
-{
-	template<int v>
-	struct vec_guard
-	{
-		static constexpr bool valid = (v >= 2 && v <= 4);
-	};
-} // namespace RF::detail
-
-namespace RF
-{
-	template<int n, typename A, RF_arithmetic_template(A)>
-	requires(detail::vec_guard<n>::valid)
-	struct vec;
-} // namespace RF
+#include "RF/maths/vec/vec.hpp"
+#include "RF/maths/vec/vec2.hpp"
+#include "RF/maths/vec/vec3.hpp"
+#include "RF/maths/vec/vec4.hpp"
--- include/RF/maths/vecs.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "RF/maths/vec.hpp"
-#include "RF/maths/vec2.hpp"
-#include "RF/maths/vec3.hpp"
-#include "RF/maths/vec4.hpp"
