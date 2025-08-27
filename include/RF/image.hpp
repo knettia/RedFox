d31767711f118541fa016a5e1abda0a21fce7492
A. C. Gäßler <debian.valken@protonmail.com>
2025-08-27 14:06:50 +0300

    Reformed image library

--- include/RF/image.hpp
+++ include/RF/image.hpp
@@ -8,26 +8,69 @@
 
 namespace RF
 {
-	enum class image_t
+	enum class image_type
 	{
+		None = 0x0,
 		KTX,
 		DDS,
 		BIT,
+		TGA, // newly added
 		PNG,
 		JPEG,
 	};
 
+	inline std::string to_string(const image_type type)
+	{
+		switch (type)
+		{
+		case (image_type::None): return "None";
+		case (image_type::KTX): return "KTX";
+		case (image_type::DDS): return "DDS";
+		case (image_type::BIT): return "BIT";
+		case (image_type::TGA): return "TGA";
+		case (image_type::PNG): return "PNG";
+		case (image_type::JPEG): return "JPEG";
+		default: return "Unknown";
+		}
+	}
+
+	enum class image_format
+	{
+		None = 0x0,
+		RGBA8,
+		RGB8,
+		RGBA16F,
+		RGBA32F,
+	};
+
+	inline std::string to_string(const image_format format)
+	{
+		switch (format)
+		{
+		case (image_format::None): return "None";
+		case (image_format::RGBA8): return "RGBA8";
+		case (image_format::RGB8): return "RGB8";
+		case (image_format::RGBA16F): return "RGBA16F";
+		case (image_format::RGBA32F): return "RGBA32F";
+		default: return "Unknown";
+		}
+	}
+
 	struct image_data_t
 	{
 		std::vector<std::byte> bytes;
-		RF::uivec2 size;
-		RF::image_t type;
+		RF::uivec2 size {};
+		RF::image_type type { RF::image_type::None };
+		RF::image_format format { RF::image_format::None };
 
+		image_data_t() = default;
 		image_data_t(const image_data_t &other) = default;
 		image_data_t &operator=(const image_data_t &other) = default;
 
-		image_data_t(const std::uint8_t *src, std::size_t n);
+		static image_data_t from_raw(const std::uint8_t *src, std::size_t n, RF::uivec2 size, RF::image_type type, RF::image_format format);
+
+		bool valid() const noexcept;
 	};
 
-	RF::image_data_t load_image(RF::image_t type, std::string_view file);
+	RF::image_data_t load_image(RF::image_type type, const std::string_view file);
 } // namespace RF
--- src/core/image.cpp
+++ src/core/image.cpp
@@ -8,97 +8,88 @@
 #include <ktx.h>
 #endif
 
-RF::image_data_t::image_data_t(const std::uint8_t *src, std::size_t n)
+#if defined(RF_DDS_SUPPORT)
+#define TINYDDSLOADER_IMPLEMENTATION
+#include <tinyddsloader.h>
+#endif
+
+RF::image_data_t RF::image_data_t::from_raw(const std::uint8_t *src, std::size_t n, RF::uivec2 size, RF::image_type type, RF::image_format format)
 {
-	this->bytes.clear();
-	this->bytes.resize(n);
-	memcpy(this->bytes.data(), src, n);
+	image_data_t img;
+	img.bytes.resize(n);
+	memcpy(img.bytes.data(), src, n);
+	img.size = size;
+	img.type = type;
+	img.format = format;
+	return img;
 }
 
-RF::image_data_t RF::load_image(RF::image_t type, std::string_view file)
+bool RF::image_data_t::valid() const noexcept
+{
+	return (!bytes.empty() && size.x > 0 && size.y > 0 && type != image_type::None && format != image_format::None);
+}
+
+RF::image_data_t RF::load_image(RF::image_type type, const std::string_view file)
 {
 	switch (type)
 	{
-		case (RF::image_t::PNG):
-		case (RF::image_t::JPEG):
-		{
-			int width, height, channels;
-			stbi_set_flip_vertically_on_load(true);
-			std::uint8_t *data = stbi_load(file.data(), &width, &height, &channels, 4);
+	case (RF::image_type::PNG):
+	case (RF::image_type::JPEG):
+	{
+		int width, height, channels;
+		stbi_set_flip_vertically_on_load(true);
+		std::uint8_t *data = stbi_load(file.data(), &width, &height, &channels, 0);
 
-			if (!data)
-			{
-				throw RF::engine_error("Failed to get image data from file '<0>' loaded with standard image type '<1>'", file, static_cast<int>(type));
-			}
+		if (!data) throw RF::engine_error("Failed to get image data from file '<0>'", file);
 
-			std::size_t data_size = width * height * 4; // 4 channels (RGBA)
+		RF::image_format format = (channels == 4) ? RF::image_format::RGBA8 : RF::image_format::RGB8;
+		std::size_t data_size = static_cast<std::size_t>(width * height * channels);
 
-			RF::image_data_t result(data, data_size);
-			result.size = RF::uivec2(width, height);
-			result.type = type;
+		image_data_t result = image_data_t::from_raw(data, data_size, RF::uivec2(width, height), type, format);
 
-			stbi_image_free(data);
-
-			return result;
-		}
-
-		case (RF::image_t::KTX):
-		{
-		#if defined (RF_KTX_SUPPORT)	
-			ktxTexture *kTexture;
-			ktx_error_code_e error_code = ktxTexture_CreateFromNamedFile(file.data(), KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT, &kTexture);
-			if(error_code != KTX_SUCCESS || !kTexture)
-			{
-				throw RF::runtime_error("Failed to load KTX texture. KTX error code: <0>", ktxErrorString(error_code));
-			}
-
-			// get image data
-			ktx_uint8_t *data = ktxTexture_GetData(kTexture);
-			ktx_size_t data_size = ktxTexture_GetDataSize(kTexture);
-			
-			// handle flipping
-			// TODO: find better way to do this
-			ktx_uint32_t row_size = kTexture->baseWidth * 4;
-
-			std::vector<ktx_uint8_t> temp_row(row_size);
-
-			ktx_uint8_t *top = data;
-			ktx_uint8_t *bottom = data + (kTexture->baseHeight - 1) * row_size;
-
-			while(top < bottom)
-			{
-				// swap rows
-				std::memcpy(temp_row.data(), top, row_size);
-				std::memcpy(top, bottom, row_size);
-				std::memcpy(bottom, temp_row.data(), row_size);
-
-				top += row_size;
-				bottom -= row_size;
-			}
-
-			// handle vector translation
-			RF::image_data_t result(data, data_size);
-			result.size = RF::uivec2(kTexture->baseWidth, kTexture->baseHeight);
-			result.type = type;
-
-			ktxTexture_Destroy(kTexture);
-
-			return result;
-		#else
-			throw RF::runtime_error("Failed to load KTX texture. RedFox Engine build was compiled without libktx");
-		#endif
-		}
-
-		case (RF::image_t::DDS):
-		{
-			// TODO: implement a dds loader
-			throw RF::engine_error("Attempt to load file of an unimplemented type", file);
-		}
-
-		case (RF::image_t::BIT):
-		{
-			// TODO: implement a simple bmp loader
-			throw RF::engine_error("Attempt to load file of an unimplemented type", file);
-		}
+		stbi_image_free(data);
+		return result;
 	}
+
+	case (RF::image_type::KTX):
+	{
+	#if defined (RF_KTX_SUPPORT)
+		ktxTexture *kTexture{};
+		ktx_error_code_e error_code = ktxTexture_CreateFromNamedFile(file.data(), KTX_TEXTURE_CREATE_LOAD_IMAGE_DATA_BIT, &kTexture);
+
+		if (error_code != KTX_SUCCESS || !kTexture) throw RF::runtime_error("Failed to load KTX texture. KTX error: <0>", ktxErrorString(error_code));
+
+		ktx_uint8_t *data = ktxTexture_GetData(kTexture);
+		ktx_size_t data_size = ktxTexture_GetDataSize(kTexture);
+
+		// For now assume RGBA8, could be extended with more precise format parsing
+		image_data_t result = image_data_t::from_raw(data, data_size, RF::uivec2(kTexture->baseWidth, kTexture->baseHeight), type, RF::image_format::RGBA8);
+
+		ktxTexture_Destroy(kTexture);
+		return result;
+	#else
+		throw RF::runtime_error("Failed to load KTX texture. RedFox Engine build was compiled without libktx");
+	#endif
+	}
+
+	case (RF::image_type::DDS):
+	{
+	#if defined(RF_DDS_SUPPORT)
+		DDSFile dds;
+		auto ret = dds.Load(file.data());
+		if (tinyddsloader::Result::Success != ret) throw RF::runtime_error("Failed to load DDS texture. TinyDDSLoader error: <0>", static_cast<int>(ret));
+
+		// TODO: finish dds format support
+		return {};
+	#else
+		throw RF::runtime_error("Failed to load DDS texture. RedFox Engine build was compiled without TinyDDSLoader");
+	#endif
+	}
+	case (RF::image_type::BIT):
+	case (RF::image_type::TGA): throw RF::engine_error("Attempt to load file of an unimplemented type <0>", RF::to_string(type));
+
+	[[fallthrough]];
+	case (RF::image_type::None): throw RF::engine_error("Attempt to load file of type 'None'");
+	}
+	return {};
 }
