e54fa23fc5027d11d177d8f8f1e97aa4f2af756c
A. C. Gäßler <debian.valken@protonmail.com>
2025-10-06 12:53:24 +0300

    Created internal_message type

--- include/RF/net/types/message.hpp
+++ include/RF/net/types/message.hpp
@@ -8,6 +8,23 @@
 
 namespace RF::net
 {
+	typedef std::uint64_t ulong_t;
+
+	enum internal_message_id_t : ulong_t
+	{
+		Application = 0,
+		Disconnect  = 1,
+		SetAgent    = 2,
+		Heartbeat   = 3
+	};
+
+	struct internal_message
+	{
+		internal_message_id_t id;
+		ulong_t app_id;
+		std::vector<RF::net::body> payload;
+	};
+
 	class message
 	{
 	private:
