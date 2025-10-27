4ff2fc384bdfc6a8f7c2d32126b46ac206b601fc
A. C. Gäßler <debian.valken@protonmail.com>
2025-10-27 17:05:26 +0200

    Dumbed down message struct

--- include/RF/net/types/message.hpp
+++ include/RF/net/types/message.hpp
@@ -25,31 +25,9 @@ namespace RF::net
 		std::vector<RF::net::body> payload;
 	};
 
-	class message
+	struct message
 	{
-	private:
-		std::uint16_t id_;
-		std::vector<RF::net::body> bodies_;
-	public:
-		message(std::uint16_t id) : id_(id), bodies_(0) { }
-
-		message(std::uint16_t id, std::span<RF::net::body> bodies) : id_(id)
-		{ bodies_.assign(bodies.begin(), bodies.end()); }
-	
-		template <typename E, RF_enum_template(E), RF_underlying_template(E, std::uint16_t)>
-		message(E msg) : id_(static_cast<std::uint16_t>(msg)) { }
-
-		template <typename E, RF_enum_template(E), RF_underlying_template(E, std::uint16_t)>
-		message(E msg, std::span<RF::net::body> bodies) : id_(static_cast<std::uint16_t>(msg))
-		{ bodies_.assign(bodies.begin(), bodies.end()); }
-		
-		~message() = default;
-
-		std::uint16_t id() const { return this->id_; }
-
-		template <typename E, RF_enum_template(E), RF_underlying_template(E, std::uint16_t)>
-		E id() const { return static_cast<E>(this->id_); }
-
-		std::span<const RF::net::body> bodies() const { return this->bodies_; }
+		ulong_t id;
+		std::vector<RF::net::body> payload;
 	};
 } // namespace RF::net
